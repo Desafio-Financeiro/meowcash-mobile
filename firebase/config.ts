7600a9e82daab530a7da1618f02f2d8c286cdde2
IsabellaSoares <isabella.soares.28@gmail.com>
2025-02-15 15:07:20 -0300

    chore: install Firebase

---
+++ firebase/config.ts
@@ -0,0 +1,34 @@
+import { FirebaseApp, getApp, getApps, initializeApp } from "firebase/app";
+import {
+  Auth,
+  getAuth,
+  initializeAuth,
+  // @ts-ignore
+  getReactNativePersistence,
+} from "firebase/auth";
+
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+};
+
+let app: FirebaseApp;
+let auth: Auth;
+
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+  auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+  });
+} else {
+  app = getApp();
+  auth = getAuth(app);
+}
+
+export { app, auth };
